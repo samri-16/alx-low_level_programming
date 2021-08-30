5594cf91933ff81e171edcd47cdeef3db1a37720
samrawit belay <samrawitbelay33@gmail.com>
2021-08-29 20:47:41 -0700

    puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - prints a text
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+	 {
+	   puts("\"programming is like building a multilingual puzzle");
+	   return (0);
+	     }
