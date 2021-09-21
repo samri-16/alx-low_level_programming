64ffa521e9193446255060f8efb19355c19a2f84
samrawit belay <samrawitbelay33@gmail.com>
2021-09-21 16:41:18 -0700

    edited in betty style

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,13 +1,16 @@
+#!/bin/bash
+gcc -wall -werror -wextra -pedantic -std=gnu89
+
 #include <stdio.h>
 /**
- * main - program prints its name
- * argc: argument count
- * argv: argument vectors
- * Return 0: success
- */
+* main - program prints its name
+* argc: argument count
+* argv: argument vectors
+* Return 0: success
+*/
 int main(int argc, char *argv[])
 {
-  argc--; /* added to pass checks */
-  printf("%s\n", argv[0]);
-  retun (0);
+argc--; /* added to pass checks */
+printf("%s\n", argv[0]);
+retun (0);
 }
